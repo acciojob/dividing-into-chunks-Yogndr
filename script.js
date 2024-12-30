484f037fe4f026934031cbfabb5ae966f1e6b05c
Yogndr <106022206+Yogndr@users.noreply.github.com>
2024-12-30 20:41:00 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,25 @@
 const arr = [1, 2, 3, 4, 1, 0, 2, 2];
-
+let ans=[];
+let temp=[];
 const divide = (arr, n) => {
+	for(let i=0;i<arr.length;i++){
+		let sum=0;
+		for(let j=i;j<arr.length;j++){
+			sum=sum+arr[j];
+			if(sum<n){
+				for(let k=i;k<=j;k++)
+					ans.push(arr[k]);
+				}
+			else if(sum>=n){
+				break;
+			}
+			temp.push(ans);
+			
+		}
+        
+	}
   // Write your code here
 };
 
-const n = prompt("Enter n: ");
+// const n = prompt("Enter n: ");
 alert(JSON.stringify(divide(arr, n)));
